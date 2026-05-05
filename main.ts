5c8986e4434a389514cf0ef6b9d957c7b789f3a7
armcreativesystems-cmd <arm.creativesystems@gmail.com>
2026-05-05 22:23:06 +0400

    NO.1

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Duck)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
