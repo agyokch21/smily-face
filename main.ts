1b356cdac86c392fe83e86bd6049dff9ab82a312
abrahamyan23 <erikabrahamyan70@gmail.com>
2026-05-16 11:43:34 +0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.QuarterNote)
+    basic.pause(2000)
     basic.showIcon(IconNames.Duck)
+    basic.pause(2000)
 })
 input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
